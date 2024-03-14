f9c295b9492d435bfd6d770eb75634144e0d26c5
Ulugbek Samigjonov <samigjonov@gmail.com>
2024-03-14 23:41:24 +0500

    feat/39: console log removed

--- src/components/layouts/dashboard-layout.tsx
+++ src/components/layouts/dashboard-layout.tsx
@@ -3,7 +3,6 @@ import Navbar from '@/components/dashboard/navbar.tsx';
 import { Outlet } from 'react-router-dom';
 
 export default function DashboardLayout() {
-  console.log(import.meta.env.VITE_OPEN_AI_KEY);
   return (
     <div className="h-screen overflow-hidden flex">
       <Sidebar />
